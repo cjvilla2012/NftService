5245ac8a360f447060c6b5553b22fbe306137381
CJ Villa <cjvilla.photo@gmail.com>
2025-05-08 09:23:43 -0600

    more

--- src/controllers/CoreServiceController.js
+++ src/controllers/CoreServiceController.js
@@ -1,6 +1,13 @@
 import axios from 'axios'
 import { logErrorWithTime, logWithTime } from '../util/controllerUtil'
-
+const getCoreServiceOptions = () => {
+    return {
+        timeout: 3000,
+        headers: {
+            'service-api-key': process.env.SERVICE_API_KEY
+        }
+    }
+}
 /**
  * Call the Core Service to retrieve the User and get its accessToken.
  * The accessToken is then only used in direct calls to the Core Service.
@@ -11,7 +18,8 @@ export const verifyUserId = async (userId) => {
     logWithTime(`verifyUser ${userId}`)
     let accessToken
     try {
-        const userResponse = await axios.get(`${process.env.USER_AUTH_API}/${userId}`, getCoreServiceOptions())
+        const userResponse = await axios.get(`${process.env.USER_AUTH_API}/${userId}`,
+             getCoreServiceOptions())
         const { data } = userResponse
         if (data) {
             logWithTime('verifyUser got user', data)
